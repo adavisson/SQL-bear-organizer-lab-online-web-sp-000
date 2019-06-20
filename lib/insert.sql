e11c32248db61fce9930015e1fd78a1223aa3a26
Andrew Davisson <akdavisson4@gmail.com>
2019-06-20 17:44:29 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,4 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Bernice", 23, "Female", "Brown", "Loose Cannon", 1)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Beatrice", 23, "Female", "Brown", "Calm", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Bernice", 23, "Female", "Brown", "Loose Cannon", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Beatrice", 16, "Female", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Barney", 17, "Male", "Black", "Aloof", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Bernice", 23, "Female", "Brown", "Loose Cannon", 1);
