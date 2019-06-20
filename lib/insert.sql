904d38babf1489991b66cfef0c31ffb45a0a19c8
Andrew Davisson <akdavisson4@gmail.com>
2019-06-20 17:47:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Bernice", 23, "Female", "Brown", "Loose Cannon", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Beatrice", 16, "Female", "Brown", "Calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Barney", 17, "Male", "Black", "Aloof", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Bernice", 23, "Female", "Brown", "Loose Cannon", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocoalate", 23, "Female", "Brown", "Loose Cannon", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 16, "Female", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 17, "Male", "Black", "Aloof", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 56, "Male", "Brown", "Loose Cannon", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 11, "Female", "Black", "Loose Cannon", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, "Male", "White", "Loose Cannon", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 21, "Female", "Brown", "Loose Cannon", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 83, "Male", "White", "Loose Cannon", 1);
